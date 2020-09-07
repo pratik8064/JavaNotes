d4c56dc30aa900986ba06429ee76b53b3f4f1d65
KiranDS89 <kiransuryawanshi89@gmail.com>
2020-09-08 04:40:19 +0530

    Finally able to added

---
+++ test/src/test/Car.java
@@ -0,0 +1,75 @@
+package test;
+
+public class Car {
+
+	private int wheelsCount;
+	private int numberOfSeats;
+	private int windows;
+	private String colour;
+
+	public static String brand = "tesla";
+
+	public final static String model = "2020";
+
+	public String printCarDetails() {
+		return this.getNumberOfSeats() + "," + this.getWheelsCount() + "," + this.getWindows() + "," + this.getColour();
+	}
+
+	Car(int wheelsCount, int numberOfSeats, int windows, String colour) {
+		this.colour = colour;
+		this.numberOfSeats = numberOfSeats;
+		this.wheelsCount = wheelsCount;
+		this.windows = windows;
+
+	}
+
+	Car() {
+
+	}
+	
+
+	public Car(int wheelsCount, int numberOfSeats, int windows) {
+		super();
+		this.wheelsCount = wheelsCount;
+		this.numberOfSeats = numberOfSeats;
+		this.windows = windows;
+	}
+
+	public int getWheelsCount() {
+		return wheelsCount;
+	}
+
+	public void setWheelsCount(int wheelsCount) {
+		this.wheelsCount = wheelsCount;
+	}
+
+	public int getNumberOfSeats() {
+		return numberOfSeats;
+	}
+
+	public void setNumberOfSeats(int numberOfSeats) {
+		this.numberOfSeats = numberOfSeats;
+	}
+
+	public int getWindows() {
+		return windows;
+	}
+
+	public void setWindows(int windows) {
+		this.windows = windows;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public static String myStaticMethod() {
+		return (brand + " in myStaticMethod ");
+
+	}
+
+}
--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,47 +5,30 @@ package test;
 
 import java.util.Arrays;
 
-/**
- * @author Pratik Suryawanshi
- *
- */
+
 class Test {	
+	
 	
 	public static void main(String[] args) {
 	
-		Student s1 = new Student();
-		
-		Student s2 = new Student("Devidas");
-		
-		Student s3 = new Student("s3", 10, "384781", "new sangavi Pune", "India", true);
-		
-		s1.setName("Pratik");
-		
-		System.out.println(s1.getName());
-
-		System.out.println(s2.getName());
-		
-		s1.setName("Kiran");
-		
-		System.out.println(s1.getName());
-		
-		System.out.println(Student.className);
-		System.out.println(s2.className);
-		
-		int a = 5;
-		int b = 6;
-		
-		System.out.println(Math.max(a, b));
-		System.out.println(s1.schoolName);
-		
-		//String details = s3.getDetails();
-		//System.out.println(details);
-		
-		System.out.println(s3.getDetails());
-		
-		//this is experiment
-		
+	Car c1 = new Car();
+	c1.setNumberOfSeats(6);
+	c1.setColour("Black");
+	c1.setWheelsCount(4);
+	c1.setWindows(6);
+	
+	System.out.println(c1.getNumberOfSeats());
+	System.out.println(c1.getColour());
+	System.out.println(c1.getWheelsCount());
+	System.out.println(c1.getWindows());
+	
+	System.out.println(c1.printCarDetails());
+	Car c2 = new Car(4,5,4,"White");
+	System.out.println(c2.printCarDetails());
+	Car c3 = new Car(4,6,4);
+	System.out.println(c3.printCarDetails());
 	}
+	
 	
 	
 
